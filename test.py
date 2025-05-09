cef88d99725d6520013a3224ac151fa7e1c9eca1
Ilham <86422270+titanilham@users.noreply.github.com>
2025-05-10 01:01:36 +0300

    upd

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import requests
 from datetime import datetime
 from time import sleep
 
-def main():
+def main() -> None:
     run = True
     while run:
         if clock() == "12:00:00":
             send_msg("hi")
             sleep(1)
     
-def clock():
+def clock() -> str:
     current = datetime.now()
     clock = current.strftime("%H:%M:%S")
     return clock
@@ -20,7 +20,6 @@ def send_msg(text: str) -> None:
     url_req = "https://api.telegram.org/bot" + TOKEN + "/sendMessage" + "?chat_id=" + CHAT_ID + "&text=" + text
     results = requests.get(url_req)
     print(results.json())
-
 
 if __name__ == "__main__":
     main()
