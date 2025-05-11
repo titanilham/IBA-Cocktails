11e683448afd73f7c39763b5f8fc26585a3f3525
Ilham <86422270+titanilham@users.noreply.github.com>
2025-05-12 02:35:25 +0300

    upd

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import random
 
 def parser():
 
-    url = "https://iba-world.com/cocktails/all-cocktails/"
-
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                     "AppleWebKit/537.36 (KHTML, like Gecko) "
